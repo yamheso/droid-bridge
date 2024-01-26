8e8829a4ea66fe2510c5450887ee7feec1ba241e
Damir <pt7mk@yandex.ru>
2024-01-27 00:47:52 +0700

    Feature/dk/dumpsys com (#13)
    * dumpsys command
    
    * added Dumpsys command

---
+++ src/main/java/commands/logcat/dumpsys/DumpsysKey.java
@@ -0,0 +1,20 @@
+package commands.logcat.dumpsys;
+
+public enum DumpsysKey {
+    ALL_SYS_SERVICES("-l"),
+    SKIP_SERVICES("--skip"),
+    TIMEOUT_SEC("-t"),
+    TIMEOUT_MS("-T"),
+    PID("--pid");
+
+
+    private String key;
+
+    DumpsysKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
